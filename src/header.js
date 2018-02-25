5214333654383837626d87d2f879c524adaea154
shuffyiosys <hotaru.hino@outlook.com>
2018-02-25 14:38:20 -0800

    Updating header and the about text.

--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       RPH Tools TEST
 // @namespace  https://openuserjs.org/scripts/shuffyiosys/RPH_Tools
-// @version    4.0.3
+// @version    4.0.4
 // @description Adds extended settings to RPH
 // @match      http://chat.rphaven.com/
 // @copyright  (c)2014 shuffyiosys@github
@@ -9,4 +9,4 @@
 // @license    MIT
 // ==/UserScript==
 
-var VERSION_STRING = '4.0.3';
+var VERSION_STRING = '4.0.4';
